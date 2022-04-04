08f971fbb2e13e68f1db7b3e73391ec64b5d47d2
shoshonstudio <pawel@shoshonstudio.com>
2022-04-04 06:34:02 +0200

    square

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
+import Canvas from './components/Canvas';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <header className="App-header">Drawing App</header>
+      <Canvas />
     </div>
   );
 }
